cf55ea065e8f3acedd4ded5e857951a25759b091
Khaetbek <khaetbek@internet.ru>
2025-11-03 17:19:49 +0500

    fix: Добавлено валидация для полей форма

--- frontend/src/app/[locale]/(admin)/admin/leads/_components/lead-content.tsx
+++ frontend/src/app/[locale]/(admin)/admin/leads/_components/lead-content.tsx
@@ -12,10 +12,10 @@ export const LeadContent = () => {
                 <div className="flex flex-col items-start justify-between gap-4 sm:flex-row sm:items-center">
                     <div>
                         <h2 className="text-3xl font-bold tracking-tight text-foreground">
-                            Задачи
+                            Лиды
                         </h2>
                         <p className="text-gray-01 mt-1">
-                            Управляйте своими задачами, конвертируйте и отслеживайте эффективность.
+                            Управляйте своими лидами, конвертируйте и отслеживайте эффективность.
                         </p>
                     </div>
 
--- frontend/src/app/[locale]/(admin)/admin/leads/_components/lead-option.tsx
+++ frontend/src/app/[locale]/(admin)/admin/leads/_components/lead-option.tsx
@@ -11,90 +11,94 @@ import { CircleEllipsis } from "lucide-react";
 import { Controller, useForm } from "react-hook-form";
 import dayjs from "dayjs";
 import { ErrorMassage } from "@/components/ui/error-message";
+import { messageSchema, type MessageSchemaType } from "@/schemas/lead.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export const LeadOption = () => {
     const { open, onOpenChange } = useOpen();
 
-    const { handleSubmit, control } = useForm<FormFields>({
-        // resolver: zodResolver(formSchema)
+    const { handleSubmit, control, formState: { errors } } = useForm<MessageSchemaType>({
+        resolver: zodResolver(messageSchema),
+        defaultValues: {
+            message: "",
+            files: [],
+        }
     });
 
-    const onSubmit = (values: FormFields) => {
-        console.log(values);
-    }
+    const onSubmit = (values: MessageSchemaType) => {
+        console.log("✅ Форм-данные:", values);
+    };
 
-    return <Sheet open={open} onOpenChange={onOpenChange}>
-        <CircleEllipsis className="hover:text-blue-500 text-2xl cursor-pointer" onClick={() => onOpenChange(true)} />
+    return (
+        <Sheet open={open} onOpenChange={onOpenChange}>
+            <CircleEllipsis
+                className="hover:text-blue-500 text-2xl cursor-pointer"
+                onClick={() => onOpenChange(true)}
+            />
 
-        <SheetContent className="max-w-5xl">
-            <SheetHeader>
-                <SheetTitle>Задача</SheetTitle>
-            </SheetHeader>
+            <SheetContent className="max-w-5xl">
+                <SheetHeader>
+                    <SheetTitle>Задача</SheetTitle>
+                </SheetHeader>
 
-            <Accordion type="single" className="w-full space-y-1.5">
-                {
-                    messages.map(message => <AccordionItem key={message.id} value={message.id + ""}>
-                        <AccordionTrigger className="cursor-pointer">
-                            {dayjs(message.create_at).format("DD.MM.YYYY HH:mm")}
-                        </AccordionTrigger>
-                        <AccordionContent>
-                            {message.message}
-                        </AccordionContent>
-                    </AccordionItem>)
-                }
+                <Accordion type="single" className="w-full space-y-1.5">
+                    {messages.map((message) => (
+                        <AccordionItem key={message.id} value={message.id + ""}>
+                            <AccordionTrigger className="cursor-pointer">
+                                {dayjs(message.create_at).format("DD.MM.YYYY HH:mm")}
+                            </AccordionTrigger>
+                            <AccordionContent>{message.message}</AccordionContent>
+                        </AccordionItem>
+                    ))}
+                </Accordion>
 
-            </Accordion>
+                <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
 
-            <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-                <Controller
-                    control={control}
-                    name="message"
-                    render={(field) => <Field>
+                    <Controller
+                        control={control}
+                        name="message"
+                        render={({ field, fieldState }) => (
+                            <Field>
+                                <CustomSunEditor defaultValue={field.value} onChange={field.onChange} />
+                                <ErrorMassage error={fieldState.error?.message} />
+                            </Field>
+                        )}
+                    />
 
-                        <CustomSunEditor defaultValue={field.field.value} onChange={field.field.onChange} />
+                    <Controller
+                        control={control}
+                        name="files"
+                        render={({ field, fieldState }) => (
+                            <Field>
+                                <FieldLabel className="text-lg" htmlFor="files">
+                                    Файл
+                                </FieldLabel>
 
-                        <ErrorMassage error={field.fieldState.error?.message} />
+                                <Input
+                                    id="files"
+                                    type="file"
+                                    sizes={"lg"}
+                                    color="light"
+                                    multiple
+                                    onChange={(e) => field.onChange(Array.from(e.target.files || []))}
+                                />
 
-                    </Field>}
-                />
+                                <ErrorMassage error={fieldState.error?.message} />
+                            </Field>
+                        )}
+                    />
 
-
-                <Field>
-
-                    <FieldLabel className="text-lg" htmlFor="files">
-                        Файл
-                    </FieldLabel>
-
-                    <Input id="files" type="file" sizes={"lg"} color="light" placeholder="Введите имя" multiple />
-
-                </Field>
-
-                <Button variant={"black"} size={"lg"}>
-                    Сохранить
-                </Button>
-            </form>
-        </SheetContent>
-    </Sheet>
+                    <Button type="submit" variant="black" size="lg">
+                        Сохранить
+                    </Button>
+                </form>
+            </SheetContent>
+        </Sheet>
+    );
 };
 
-interface FormFields {
-    message: string;
-}
-
 const messages = [
-    {
-        id: 1,
-        create_at: new Date(),
-        message: "Привет"
-    },
-    {
-        id: 2,
-        create_at: new Date(),
-        message: "Привет"
-    },
-    {
-        id: 3,
-        create_at: new Date(),
-        message: "Привет"
-    }
+    { id: 1, create_at: new Date(), message: "Привет 1" },
+    { id: 2, create_at: new Date(), message: "Привет 2" },
+    { id: 3, create_at: new Date(), message: "Привет 3" },
 ] as const;
