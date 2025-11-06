e11055ac2298f657f4dcb8d17480e325b1d6421f
Shukhratbekov Boburbek <shukhratbekovb@gmail.com>
2025-11-06 16:19:31 +0500

    fix: Authorization

--- backend/src/routers/lead_status.py
+++ backend/src/routers/lead_status.py
@@ -25,7 +25,7 @@ router = APIRouter(
 @cbv(router)
 class LeadStatusCBV:
     db: AsyncSession = Depends(get_db)
-
+    user: User = Depends(get_current_user)
     @router.get(
         "/",
         response_model=Page[LeadStatusRead],
