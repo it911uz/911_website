69e7d5b646f00069910085d44ea9b346768e4466
Khaetbek <khaetbek@internet.ru>
2025-11-10 13:41:31 +0500

    fix: подготовлено страница акции и поправлено ключи файлов

--- frontend/src/app/[locale]/(client)/promotion/page.tsx
+++ frontend/src/app/[locale]/(client)/promotion/page.tsx
@@ -1,12 +1,17 @@
-import { ComingSoon } from "@/components/ui/coming-soon";
 import type { Locale } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
+import { Header } from "./_components/header";
+import { SectionPromo } from "./_components/content";
 
 const Page = async ({ params }: PageProps<"/[locale]/promotion">) => {
     const { locale } = await params;
     setRequestLocale(locale as Locale);
 
-    return <ComingSoon />
+    return <>
+        <Header />
+
+        <SectionPromo />
+    </>
 }
 
 export default Page;
