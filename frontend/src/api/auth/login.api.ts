fac4e5fb9e3fe37a06c0d64906b472b784643f14
Shukhratbekov Boburbek <shukhratbekovb@gmail.com>
2025-12-12 17:40:35 +0500

    Update login.api.ts

--- frontend/src/api/auth/login.api.ts
+++ frontend/src/api/auth/login.api.ts
@@ -2,6 +2,7 @@ import { Env } from "@/configs/env.config";
 import type { JWTType } from "@/types/jwt.type";
 
 export const login = async ({ body }: Props) => {
+    console.log("Login API called");
     console.log("API URL:", Env.PUBLIC_API_URL);
     const response = await fetch(Env.PUBLIC_API_URL + "/auth/token", {
         method: "POST",
