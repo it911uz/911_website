2d1d117b99490390c27876233d3ff664f8942bf2
Shukhratbekov Boburbek <shukhratbekovb@gmail.com>
2025-12-12 17:11:46 +0500

    Update env.config.ts

--- frontend/src/configs/env.config.ts
+++ frontend/src/configs/env.config.ts
@@ -1,5 +1,5 @@
 export const Env = {
-    PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL!,
+    PUBLIC_API_URL: "http://fastapi-app:8000",
     AUTH_SECRET: process.env.AUTH_SECRET!,
     NODE_ENV: process.env.NODE_ENV || "production",
 } as const;
