6fa7227594772f59726403112767dea4f52d8ff9
Khaetbek <khaetbek@internet.ru>
2025-12-12 16:56:09 +0500

    fix: headers

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -82,44 +82,28 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  headers: async () => [
-    {
-      source: "/(.*)",
-      headers: [
-        { key: "X-Developed-By", value: "IT 911" },
-        { key: "X-Frame-Options", value: "DENY" },
-        { key: "X-Content-Type-Options", value: "nosniff" },
-        { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-        {
-          key: "Permissions-Policy",
-          value: "camera=(), microphone=(), geolocation=(), interest-cohort=()"
-        },
-        {
-          key: "Strict-Transport-Security",
-          value: "max-age=63072000; includeSubDomains; preload"
-        },
-        {
-          key: "Content-Security-Policy",
-          value: `
-            default-src 'self';
-            script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
-            style-src 'self' 'unsafe-inline' https:;
-            img-src 'self' blob: data: https:;
-            font-src 'self' https:;
-            connect-src 'self' https:;
-            frame-ancestors 'none';
-          `.replace(/\n/g, "")
-        }
-      ],
-    },
-    {
-      source: "/:path*{/}?",
-      headers: [
-        { key: "X-Accel-Buffering", value: "no" },
-        { key: "Cache-Control", value: "no-store" }
-      ],
-    }
-  ],
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Developed-By",
+            value: "IT 911",
+          },
+        ],
+      },
+      {
+        source: "/:path*{/}?",
+        headers: [
+          {
+            key: "X-Accel-Buffering",
+            value: "no",
+          },
+        ],
+      }
+    ];
+  },
 };
 
 const withNextIntl = createNextIntlPlugin({
