a7349e560fe6b21159de044e9365a0b78d720eac
Khaetbek <khaetbek@internet.ru>
2026-01-15 17:11:29 +0500

    fix: обновлено пакеты и новости

--- frontend/src/app/[locale]/(client)/page.tsx
+++ frontend/src/app/[locale]/(client)/page.tsx
@@ -1,12 +1,12 @@
-import { setRequestLocale } from "next-intl/server";
-import { News } from "./_components/news";
 import type { Locale } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
 import { About } from "./_components/about";
 import { Clients } from "./_components/clients";
-import { Success } from "./_components/success";
-import { Statistics } from "./_components/statistics";
-import { Today } from "./_components/today";
 import { Footer } from "./_components/footer";
+import { News } from "./_components/news";
+import { Statistics } from "./_components/statistics";
+import { Success } from "./_components/success";
+import { Today } from "./_components/today";
 
 const Page = async ({ params }: PageProps<"/[locale]">) => {
 	const { locale } = await params;
